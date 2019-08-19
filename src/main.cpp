112f20659741afa37db71dd4c7a9ddbd269bfd78
Guilherme <guilhermedfs11@gmail.com>
2019-08-18 22:41:46 -0300

    Created main menu.

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+	cout<< "             Bem-vindo ao programa de gerenciamento de vendas."<<endl<<endl;
+
+	cout<< "1- Modo venda"<<endl;
+	cout<< "2- Modo recomendação"<<endl;
+	cout<< "3- Modo estoque"<<endl;
+
+	cout<< "Selecione um dos modos acima: "<<endl<<endl;
+
+
+	return 0;
+}
